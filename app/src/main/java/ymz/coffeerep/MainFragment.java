531d0609ad0ab3e48c524fdedb94c8b266eee64d
yama-8080 <yamazaki.k.bc@m.titech.ac.jp>
2024-07-10 02:47:43 +0900

    add default android app

---
+++ app/src/main/java/ymz/coffeerep/MainFragment.java
@@ -0,0 +1,9 @@
+package ymz.coffeerep;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+    public MainFragment(){
+        super(R.layout.fragment_main);
+    }
+}
