89a6d2ddb7e32f45483a2659ceb98b8ca02ff4a5
yama-8080 <yamazaki.k.bc@m.titech.ac.jp>
2024-07-10 04:29:51 +0900

    implement scene transition sample

---
+++ app/src/main/java/ymz/coffeerep/scenes/detail/DefaultCoffeeReviewDetail.java
@@ -0,0 +1,11 @@
+package ymz.coffeerep.scenes.detail;
+
+import androidx.fragment.app.Fragment;
+import ymz.coffeerep.R;
+
+public class DefaultCoffeeReviewDetail extends Fragment {
+
+    public DefaultCoffeeReviewDetail() {
+        super(R.layout.fragment_default_detail_cr);
+    }
+}
---
+++ app/src/main/java/ymz/coffeerep/scenes/register/DefaultCoffeeReviewRegister.java
@@ -0,0 +1,11 @@
+package ymz.coffeerep.scenes.register;
+
+import androidx.fragment.app.Fragment;
+import ymz.coffeerep.R;
+
+public class DefaultCoffeeReviewRegister extends Fragment {
+
+    public DefaultCoffeeReviewRegister() {
+        super(R.layout.fragment_default_register_cr);
+    }
+}
