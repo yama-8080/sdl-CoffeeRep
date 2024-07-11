ef3de9b22a3771e211481f5427576dfb2139e4d0
yama-8080 <yamazaki.k.bc@m.titech.ac.jp>
2024-07-11 16:23:50 +0900

    resolve database/repositoryOld problem: just mistake in method type

--- app/src/main/java/ymz/coffeerep/data/raw_beans/RawBeansDatabaseOld.java
+++ app/src/main/java/ymz/coffeerep/data/raw_beans/RawBeansDatabaseOld.java
@@ -1,29 +1,36 @@
 package ymz.coffeerep.data.raw_beans;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Database(entities = {RawBeans.class}, version = 1, exportSchema = false)
 public abstract class RawBeansDatabaseOld extends RoomDatabase {
-    /*
+
     public abstract RawBeansDAO rawbeansDao();
 
-    private static volatile RawBeansDatabase INSTANCE;
+    private static volatile RawBeansDatabaseOld INSTANCE;
     private static final int NUMBER_OF_THREADS = 4;
-    static final ExecutorService databaseWriteExecutor =
+
+    public static final ExecutorService databaseWriteExecutor =
             Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
-    static WordRoomDatabase getDatabase(final Context context) {
+    public static RawBeansDatabaseOld getDatabase(final Context context) {
         if (INSTANCE == null) {
-            synchronized (RawBeansDatabase.class) {
+            synchronized (RawBeansDatabaseOld.class) {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                                    RawBeansDatabase.class, "rawbeans_db")
-                            .build();
+                                    RawBeansDatabaseOld.class, "rawbeans_db")
+                                    .build();
                 }
             }
         }
         return INSTANCE;
     }
-    */
+
 }
--- app/src/main/java/ymz/coffeerep/repository/raw_beans/RawBeansRepositoryOld.java
+++ app/src/main/java/ymz/coffeerep/repository/raw_beans/RawBeansRepositoryOld.java
@@ -1,13 +1,24 @@
 package ymz.coffeerep.repository.raw_beans;
 
+import android.app.Application;
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import ymz.coffeerep.data.raw_beans.RawBeans;
+import ymz.coffeerep.data.raw_beans.RawBeansDAO;
+import ymz.coffeerep.data.raw_beans.RawBeansDatabaseOld;
+
 public class RawBeansRepositoryOld {
-    /*
+
     private RawBeansDAO mRawbeansDao;
     private LiveData<RawBeans> mRawbeans;
     private int flag = 0;
 
     public RawBeansRepositoryOld(Application application) {
-        RawBeansDatabase db = RawBeansDatabase.getDatabase(application);
+        RawBeansDatabaseOld db = RawBeansDatabaseOld.getDatabase(application);
         mRawbeansDao = db.rawbeansDao();
     }
 
@@ -17,9 +28,9 @@ public class RawBeansRepositoryOld {
     }
 
     public void insert(RawBeans rawbeans){
-        RawBeansDatabase.databaseWriteExecutor.execute(() -> {
+        RawBeansDatabaseOld.databaseWriteExecutor.execute(() -> {
             mRawbeansDao.insertRawBeans(rawbeans);
         });
     }
-    */
+
 }
