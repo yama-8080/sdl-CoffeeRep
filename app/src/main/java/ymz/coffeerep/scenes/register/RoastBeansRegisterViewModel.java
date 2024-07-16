613059418867f5e0c9e00cb424cef9c545fe88e9
yama-8080 <yamazaki.k.bc@m.titech.ac.jp>
2024-07-17 02:14:20 +0900

    add unselected-state to dynamic-dropdown-items

--- app/src/main/java/ymz/coffeerep/scenes/register/RoastBeansRegisterViewModel.java
+++ app/src/main/java/ymz/coffeerep/scenes/register/RoastBeansRegisterViewModel.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import ymz.coffeerep.data.dropdown.DropDownDefault;
 import ymz.coffeerep.data.rawbeans.RawBeans;
 import ymz.coffeerep.data.roastbeans.RoastBeans;
 import ymz.coffeerep.repository.rawbeans.RawBeansRepository;
@@ -40,6 +41,11 @@ public class RoastBeansRegisterViewModel extends AndroidViewModel {
 
     //set items to dropdown list
     void setRoastRawbeansItemsToAdapter(ArrayAdapter<String> adapter, List<RawBeans> rawbeans){
+        //default item
+        adapter.add(DropDownDefault.getDefault_str());
+        _allRawBeansId.add(DropDownDefault.getDefault_id());
+
+        //read all items
         Iterator<RawBeans> iterator = rawbeans.iterator();
         while (iterator.hasNext()) {
             RawBeans item = iterator.next();
